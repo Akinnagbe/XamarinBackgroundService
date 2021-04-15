89679af3af692b5ce137f686d0881419c7855fb6
Olamide james <olamidejames007@gmail.com>
2021-04-15 22:39:08 +0100

    initial commit

---
+++ XamarinBackgroundingService.Android/SampleBackgroundService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Android.Content;
+using AndroidX.Work;
+
+namespace XamarinBackgroundingService.Droid
+{
+    public class SampleBackgroundService: Worker
+    {
+        public SampleBackgroundService(Context context, WorkerParameters workerParameters) : base(context, workerParameters)
+        {
+        }
+
+        public override Result DoWork()
+        {
+            var taxReturn = GetTime();
+            Android.Util.Log.Debug("Olamide", $"The time is : {taxReturn}");
+            return Result.InvokeSuccess();
+        }
+
+        public string GetTime()
+        {
+            return DateTime.Now.ToString();
+        }
+
+
+    }
+}
